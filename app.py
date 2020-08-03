873a1931864847a38597ae97894931844d1cd6f9
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-08-03 19:59:28 +0530

    Fixed errors with coloring on clicks

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    arr = [1, 2, 3, 4, 5, 6, 7]
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     return render_template('graph_vis.html', path_graph=arr)
 
 
