85447c8bdce45364c46833e49a7974f761974947
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-08-04 20:13:45 +0530

    Now sending the js array to flask, also cleaned repo

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello_world():
-    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-    return render_template('graph_vis.html', path_graph=arr)
+    if request.method == "POST":
+        new_arr = request.form.getlist('new_state[]')
+        print(f"From Python: {new_arr}")
+
+    else:
+        arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+        return render_template('graph_vis.html', path_graph=arr)
 
 
 if __name__ == '__main__':
