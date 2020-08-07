b46aefbaa88b588e5f07bddd26896f53b253ed6a
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-08-07 13:54:05 +0530

    Re-wrote algo in JavaScript:

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from algorithms import PathGraph
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 graph = PathGraph(5, 3)
 arr = [0 for i in range(5)]
-attack_edge = (-1, -1)
+attack_edge = [-1, -1]
 
 
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
-    global arr
+    global arr, attack_edge
     if request.method == "POST":
         arr = request.form.getlist('new_state[]')
         print(f"From Python: {arr}")
-
+        attack_edge = list(graph.attackerAI(arr))
+        print("Redirecting")
+        return redirect('/')
+        # return render_template('graph_vis.html', path_graph=arr, next_edge=attack_edge)
     else:
-        return render_template('graph_vis.html', path_graph=arr, point_to_edge=attack_edge)
+        print("In Get")
+        print(f"Sending: {attack_edge}")
+        return render_template('graph_vis.html', path_graph=arr, next_edge=attack_edge)
 
     return 'OK'
 
--- static/script.js
+++ static/script.js
@@ -1,36 +1,38 @@
-/*let nodes = new vis.DataSet([
-    {id: 1, label: 1},
-    {id: 2, label: 2},
-    {id: 3, label: 3},
-    {id: 4, label: 4},
-    {id: 5, label: 5}
-]);
-
-let edges = new vis.DataSet([
-    {from: 1, to: 2},
-    {from: 2, to: 3},
-    {from: 3, to: 4},
-    {from: 4, to: 5},
-]);
-
-let tile = document.getElementById("network_area");
-
-let data = {
-    nodes: nodes,
-    edges: edges
-}
-
-let network = new vis.Network(tile, data, {});
-
-network.on("click", function(cNode) {
-    let nodeId = cNode['nodes']['0'];
-    if (nodeId) {
-        let clickedNode = nodes.get(nodeId);
-        clickedNode.color = {
-            border: '#000000',
-            background: '#ff0000'
-        }
-        nodes.update(clickedNode);
+class PathGraph{
+    constuctor(size, initial_guards) {
+        this.vertices = size
+        this.edges = size - 1;
+        this.guards = initial_guards;
+        this.eternal_vertex_cover = this.vertices;
+        if (this.eternal_vertex_cover >= this.vertices - 1)
+            this.winner = "defender";
+        else
+            this.winner = "";
     }
-}); */
 
+    oneStepWin(current_state){
+        for (let i = 0; i < this.vertices; i++) {
+            if (i > 0 && (current_state[i] === 0 && current_state[i - 1] === 0)) {
+                this.winner = "attacker";
+                return [i - 1, i];
+            }
+        }
+        return [-1, -1];
+    }
+
+    attackerAI (current_state){
+        status = this.oneStepWin();
+        if (this.winner !== "")
+            return [0, 0];
+
+        for (let i = 0; i < this.vertices - 1; i++) {
+            if (current_state[i] === 1 && (current_state[i - 1] === 0 && current_state[i + 1] == 0))
+                return [i, i + 1]
+        }
+        for (let i = 0; i < this.vertices; i++) {
+            if (current_state[i] === 0)
+                return [i - 1, i];
+        }
+        return [-1, -1];
+    }
+}
