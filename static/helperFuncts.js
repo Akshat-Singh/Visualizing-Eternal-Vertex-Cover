67df6b5cab19a41df1e26759166e63aa86a05380
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-08-10 15:30:54 +0530

    Improved documentation

--- static/helperFuncts.js
+++ static/helperFuncts.js
@@ -1,22 +1,30 @@
 /* Handling the submission of a particular graph state */
 function submitTurn() {
+    /* Un-highlight the previously highlighted edge */
     unhighlightPrevious(attack_edge[0], attack_edge[1]);
 
+    /* Iterate over the entire graph to compute the final state */
     let length = graph.length;
-    let finalState = [];
+    let final_state = [];
     let i;
     for (i = 1; i <= length; i++) {
         let currNode = nodes.get(i);
-        if (currNode.color === colors[1]) {
-            finalState.push(1);
-        } else {
-            finalState.push(0);
-        }
+        if (currNode.color === colors[1])
+            final_state.push(1);
+        else
+            final_state.push(0);
+
     }
-    alert(finalState);
-    attack_edge = datStruct.attackerAI(finalState);
+    alert(final_state);
+
+    /* Call the attackerAI on the final state to see which edge will be next */
+    attack_edge = datStruct.attackerAI(final_state);
     alert(attack_edge);
+
+    /* Highlight that edge */
     pointToEdge(attack_edge[0], attack_edge[1]);
+
+    /* Update the status bar */
     document.getElementById("attack_bar").innerText = "Attacked Edge: " + attack_edge;
 }
 /* ==================================================== */
@@ -24,9 +32,14 @@ function submitTurn() {
 
 /* Helper function to point to a particular edge on the graph */
 function pointToEdge(node1, node2) {
+    /* Get the edge by serializing the associated node IDs */
     let assocEdge = edges.get(node1.toString() + "," + node2.toString());
+
+    /* Change the color, width of the edge */
     assocEdge.color = {color: "#ff0000"};
     assocEdge.width = 5;
+
+    /* Update the edge by plugging it back to the network */
     edges.update(assocEdge);
 }
 /* =========================================================== */
@@ -34,10 +47,14 @@ function pointToEdge(node1, node2) {
 
 /* Helper function to undo the highlight made by the previous function after the attack is ceased */
 function unhighlightPrevious(node1, node2) {
-    alert(node1.toString() + "," + node2.toString());
+    /* Get the edge by serializing the associated node IDs */
     let prevEdge = edges.get(node1.toString() + "," + node2.toString());
+
+    /* Reset the color, width of the edge */
     prevEdge.color = {color: "#000000"};
     prevEdge.width = 1;
+
+    /*Update the edge by plugging it back into the network */
     edges.update(prevEdge);
 }
 /* =============================================================================================== */
