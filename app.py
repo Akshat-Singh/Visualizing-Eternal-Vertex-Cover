57d9cf4892cf0aebdf321fadc72151ee23b99c6b
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-08-15 00:05:49 +0530

    Removed redundancies

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template, request, redirect
-from algorithms import PathGraph
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
-graph = PathGraph(5, 3)
-arr = [0 for i in range(5)]
-attack_edge = [-1, -1]
 
 
 @app.route('/', methods=["GET", "POST"])
